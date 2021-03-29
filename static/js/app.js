1dfc18a8479fd76a241f22e2d6672b81a7f65911
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-03-29 10:53:32 -0400

    Starter code for app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn")
+
+// Select the form
+var form = d3.select("#form")
+
+// Create event handlers
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+// Event handler function for the form
+function runFilter () {
+
+    // Prevent page from automatically refreshing
+    d3.event.preventDefault();
+
+    // Select input element
+    var inputElement = d3.select("#datetime");
+
+    // Get value property of input element
+    var inputValue = inputElement.property("value");
+}
